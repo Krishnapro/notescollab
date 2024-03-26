249ffc9f3fce11b61cf89f7c299597ae8806f1e4
Krishna Kumar <krishnachaurasia1998@gmail.com>
2024-03-26 23:17:39 +0530

    add password encoder
    Signed-off-by: Krishna Kumar <krishnachaurasia1998@gmail.com>

--- src/main/java/com/notescollab/notescollab/config/SecurityConfig.java
+++ src/main/java/com/notescollab/notescollab/config/SecurityConfig.java
@@ -7,6 +7,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.net.http.HttpRequest;
@@ -29,6 +32,11 @@ public class SecurityConfig {
 
         return http.build();
 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder ();
     }
 
 
