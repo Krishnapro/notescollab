745323df52689e28e0e6b6b20b6ea71822ab4678
Krishna Kumar <krishnachaurasia1998@gmail.com>
2024-04-01 23:54:22 +0530

    hadle exception while user not found
    Signed-off-by: Krishna Kumar <krishnachaurasia1998@gmail.com>

--- src/main/java/com/notescollab/notescollab/controller/UserController.java
+++ src/main/java/com/notescollab/notescollab/controller/UserController.java
@@ -5,14 +5,18 @@ import com.notescollab.notescollab.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationObservationContext;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestController
 @RequestMapping("/api")
@@ -32,13 +36,21 @@ public class UserController {
     }
     @GetMapping("/getuserdetails/{userid}")
     @PreAuthorize ("hasRole('ROLE_USER')")
-    public User getUserDetails(@PathVariable("userid") Long userid) throws Exception {
+    public ResponseEntity<?> getUserDetails(@PathVariable("userid") Long userid) throws Throwable {
         try {
 
             logger.info ( "Getting user details..." + userid );
-            return userRepository.getUserById ( userid );
-        }catch (Exception e){
-            throw new Exception ( "Could not get user details"+e.getMessage (),e);
+            User user = userRepository.getUserById ( userid );
+            if(user == null){
+                throw new UsernameNotFoundException ( "user " + userid + " does not found");
+            }
+            return ResponseEntity.ok (user);
+        }catch (UsernameNotFoundException e) {
+            throw e;
+        }catch(Exception e){
+            logger.info("getUserDetails:: Exception while getting user details"+e.getMessage(),e);
+            return ResponseEntity.ok ("User "+userid+" does not found");
+
         }
     }
 
