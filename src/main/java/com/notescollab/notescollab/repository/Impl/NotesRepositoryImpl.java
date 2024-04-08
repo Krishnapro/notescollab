ca905eaa10f61ec8a01db36ef3364e99330618f7
Krishna Kumar <krishnachaurasia1998@gmail.com>
2024-04-09 00:15:34 +0530

    notes repository and impl created
    Signed-off-by: Krishna Kumar <krishnachaurasia1998@gmail.com>

---
+++ src/main/java/com/notescollab/notescollab/repository/Impl/NotesRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.notescollab.notescollab.repository.Impl;
+
+import com.notescollab.notescollab.entity.NotesDetails;
+import com.notescollab.notescollab.repository.NotesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+
+@Repository
+public class NotesRepositoryImpl implements NotesRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    private static final String ADD_NOTES_QUERY = "INSERT INTO public.notes (title,description, notescontent,userid) VALUES(?,?,?,?)";
+
+
+    @Override
+    public String createNotes(NotesDetails notes, Long userId) {
+
+        KeyHolder keyHolder = new GeneratedKeyHolder ();
+        jdbcTemplate.update(conn -> {
+            PreparedStatement ps = conn.prepareStatement(ADD_NOTES_QUERY, new String[]{"id"});
+            ps.setString(1, notes.getTitle ());
+            ps.setString ( 2, notes.getDescription ());
+            ps.setString ( 3, notes.getNotesContents ());
+            ps.setLong ( 4, userId);
+
+            return ps;
+        }, keyHolder);
+
+
+        int generatedId = keyHolder.getKey().intValue ();
+
+        return "Notes successfully created with id- " + generatedId;
+    }
+}
---
+++ src/main/java/com/notescollab/notescollab/repository/NotesRepository.java
@@ -0,0 +1,8 @@
+package com.notescollab.notescollab.repository;
+
+import com.notescollab.notescollab.entity.NotesDetails;
+
+public interface NotesRepository {
+
+ String createNotes(NotesDetails notes, Long userId);
+}
