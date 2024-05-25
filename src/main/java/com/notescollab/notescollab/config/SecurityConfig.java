2867d7454b8ac26f041f978129a444e58c0fb159
Krishna Kumar <krishnachaurasia1998@gmail.com>
2024-05-25 23:17:37 +0530

    changes to api endpoint authentication permit
    Signed-off-by: Krishna Kumar <krishnachaurasia1998@gmail.com>

--- src/main/java/com/notescollab/notescollab/config/SecurityConfig.java
+++ src/main/java/com/notescollab/notescollab/config/SecurityConfig.java
@@ -33,8 +33,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers ("/api/auth/**").permitAll ()
-                        .requestMatchers ( "/api/greeting" ).permitAll ()
+                        .requestMatchers ("/api/auth/**","/api/greeting").permitAll ()
                         .anyRequest ()
                         .authenticated ())
                 .httpBasic (Customizer.withDefaults () )
