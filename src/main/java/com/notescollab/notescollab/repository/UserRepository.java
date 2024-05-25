85b42cf99ed142f642cc80b9cd36eb919bd3f427
Fabio Menna <menna.fabio@outlook.com>
2024-05-25 23:13:16 +0200

    feat: adding findByUsernameAndEmail to repository
    Signed-off-by: Fabio Menna <menna.fabio@outlook.com>

--- src/main/java/com/notescollab/notescollab/repository/UserRepository.java
+++ src/main/java/com/notescollab/notescollab/repository/UserRepository.java
@@ -12,4 +12,5 @@ public interface UserRepository {
     String loginUser(MyUser user);
 
     Optional<MyUser> findByUsername(String username);
+    Optional<MyUser> findByUsernameAndEmail(String username, String emailid);
 }
