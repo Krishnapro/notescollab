cdf53652026da110a3eca741d03ad43e05f37785
Krishna Kumar <krishnachaurasia1998@gmail.com>
2024-06-09 10:26:52 +0530

    fix: #5 add jwt token generation and jwt authentication
    Signed-off-by: Krishna Kumar <krishnachaurasia1998@gmail.com>

--- src/main/java/com/notescollab/notescollab/NotescollabApplication.java
+++ src/main/java/com/notescollab/notescollab/NotescollabApplication.java
@@ -1,9 +1,12 @@
 package com.notescollab.notescollab;
 
+import com.notescollab.notescollab.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SecurityConfig.RsaKeyProperties.class)
 public class NotescollabApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/notescollab/notescollab/config/SecurityConfig.java
+++ src/main/java/com/notescollab/notescollab/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.notescollab.notescollab.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import com.notescollab.notescollab.services.UserInfoDetailSevice;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -16,12 +23,22 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebSecurity
@@ -30,6 +47,12 @@ public class SecurityConfig {
 
     private Logger logger = LoggerFactory.getLogger ("com.notescollab.notescollab.config.SecurityConfig");
 
+    private final RsaKeyProperties rsaKeys;
+
+    public SecurityConfig(RsaKeyProperties rsaKeys) {
+        this.rsaKeys = rsaKeys;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
@@ -37,6 +60,8 @@ public class SecurityConfig {
                         .requestMatchers ("/api/auth/**","/api/greeting").permitAll ()
                         .anyRequest ()
                         .authenticated ())
+                .oauth2ResourceServer (oauth2 -> oauth2.jwt (jwtCoverter ->
+                        jwtCoverter.jwtAuthenticationConverter (jwtAuthenticationConverter())))
                 .httpBasic (Customizer.withDefaults () )
                 .formLogin (form ->  form
                         .loginPage ("/login")
@@ -79,5 +104,32 @@ public class SecurityConfig {
         authenticationProvider.setPasswordEncoder ( passwordEncoder () );
         return authenticationProvider;
 
+    }
+
+    @ConfigurationProperties(prefix = "rsa")
+    public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey){
+
+    }
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<> (new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("roles"); // Customize based on your JWT claims
+        grantedAuthoritiesConverter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
     }
 }
